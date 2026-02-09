4f1b2cb5bc770293cbdf121cc260a2578f1d0bed
auroregql <aurore.gicquel.06@gmail.com>
2026-02-09 13:51:08 +0000

    Exercice 3 terminé : tableau imbriqués php dans html

---
+++ affichage/exercice3/index.php
@@ -0,0 +1,35 @@
+<?php
+    $users = [
+        [
+            "firstName" => "Bugs",
+            "lastName" => "Bunny"
+        ],
+        [
+            "firstName" => "Roger",
+            "lastName" => "Rabbit"
+        ]
+    ];
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 3</title>
+    </head>
+    <body>
+        <h1>
+            Liste des utilisateurs
+        </h1>
+        <ul>
+            <?php foreach ($users as $user): ?>
+                <p>
+                <?php foreach ($user as $key => $value): ?>
+                    <?= "$key : $value <br>" ?>
+                <?php endforeach; ?>
+                </p>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
+
